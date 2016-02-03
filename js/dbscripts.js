fbdfba2ef6a7e4a3a5d15e7feed564caf7169f89
Abby Rolling and Daren Schaad <arolling@gmail.com>
2016-02-03 10:11:35 -0800

    Make separate readout for filtered list that takes a human object

--- js/dbscripts.js
+++ js/dbscripts.js
@@ -4,22 +4,7 @@
  * @license MIT {@link http://opensource.org/licenses/MIT}.
  */
 
- // person object for testing
- var human = new Person('Bob', 'Smith', 55);
- //var humans = [];
- human.animalType.push("Cat");
- human.animalType.push("Lizard");
- human.animalType.push("Mouse");
- human.animalType.push('Snake');
- human.animalType.push("Rabbit");
- human.animalType.push("Dog");
- human.animalType.push('Hamster');
- //human.activeDocile = 'Active';
- human.introvertedExtroverted = "";
- human.budget = 100;
- //humans.push(human);
- //console.log(humans);
- //end test object
+
 
 var filterResults = function(pets, human) { // expects pets array, human object
   var remainingAnimals = [];
@@ -86,14 +71,31 @@ function showProps(obj, objName) {
         result += obj[i] + "<br>";
     }
   }
-  console.log(result);
+  // console.log(result);
   return result;
 }
 
 window.onload = function() {
+  // person object for testing
+  var human = new Person('Bob', 'Smith', 55);
+  //var humans = [];
+  human.animalType.push("Cat");
+  human.animalType.push("Lizard");
+  human.animalType.push("Mouse");
+  human.animalType.push('Snake');
+  human.animalType.push("Rabbit");
+  //human.animalType.push("Dog");
+  human.animalType.push('Hamster');
+  //human.activeDocile = 'Active';
+  human.introvertedExtroverted = "";
+  human.budget = 100;
+  //humans.push(human);
+  //console.log(humans);
+  //end test object
 
   // Display the todo items.
   petDB.open(refreshPets);
+  //petDB.open(matchPets(human));
 
   // // Get references to the form elements.
   var newPetForm = document.getElementById('new-pet-form');
@@ -122,6 +124,7 @@ window.onload = function() {
       console.log(animal);
       petDB.createPet(animal, function(pet) {
         refreshPets();
+        matchPets(human);
       });
       newPetName.value = '';
       newPetWeight.value = '';
@@ -147,10 +150,50 @@ function refreshPets() {
     var petList = document.getElementById('pet-items');
     petList.innerHTML = '';
     console.log(pets);
+
+    for(var i = 0; i < pets.length; i++) {
+      // Read the array items backwards (most recent first).
+      var pet = pets[(pets.length - 1 - i)];
+      var li = document.createElement('li');
+      var checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+      checkbox.className = "pet-checkbox";
+      checkbox.setAttribute("data-id", pet.timestamp);
+
+      li.appendChild(checkbox);
+
+      var span = document.createElement('span');
+      var image = document.createElement('img');
+      var petProps = showProps(pet.animalObject, 'pet.animalObject');
+      span.innerHTML = petProps;
+
+      image.setAttribute('src', pet.animalObject.profilePic);
+
+      li.appendChild(span);
+      li.appendChild(image);
+
+      petList.appendChild(li);
+
+      // Setup an event listener for the checkbox.
+      checkbox.addEventListener('click', function(e) {
+        var id = parseInt(e.target.getAttribute('data-id'));
+
+        petDB.deletePet(id, refreshPets);
+      });
+    }
+  });
+}
+
+function matchPets(human) {
+  petDB.fetchPets(function(pets) {
+
+    var petList = document.getElementById('filtered-items');
+    petList.innerHTML = '';
+    console.log(pets);
     var filteredPets = filterResults(pets, human); //returns an array of animal objects that match criteria
     for(var i = 0; i < filteredPets.length; i++) {
       // Read the array items backwards (most recent first).
-      var pet = filteredPets[(filteredPets.length - 1 - i)];
+      var pet = filteredPets[i];
       var li = document.createElement('li');
       var checkbox = document.createElement('input');
       checkbox.type = "checkbox";
